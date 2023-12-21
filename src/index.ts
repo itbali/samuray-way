86d5b1fc9692571379315c77ec3b85121f81d1f5
dyuzhev <alex.itbali@gmail.com>
2023-12-21 21:09:48 +0300

    changed port2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express"
 import * as process from "process";
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 
